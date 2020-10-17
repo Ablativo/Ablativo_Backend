dc8ef0b482c9dc1ffb9d41737f2e6d92dc67d0fe
valecor95 <coretti.1635747@studenti.uniroma1.it>
2020-10-17 16:22:17 +0200

    Added first part for music_generation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,6 +8,15 @@ const path = require("path");
 
 const { v4: uuidv4 } = require("uuid");
 
+const Device = require("../models/device.model.aws");
+
+
+//MUSIC MODULES
+const mvae = require('@magenta/music/node/music_vae');
+const core = require('@magenta/music/node/core');
+const MidiWriter = require("midi-writer-js");
+
+
 //DONE
 
 exports.getMyInfo = (req, res) => {
@@ -112,6 +121,46 @@ exports.createVisit = (req, res) => {
     console.log(req.decoded._id + " DEBUG END: createVisit");
   } catch (e) {
     console.error(req.decoded._id + " CATCH: createVisit : user error > " + e);
+    return res.send({ success: false, status: 500, message: e.message });
+  }
+};
+
+//////// WORK IN PROGRESS ////////
+exports.endVisit = (req, res) => {
+  try {
+    console.log(req.decoded._id + " DEBUG START: endVisit");
+    var userId = req.decoded._id;
+    now = Date.now()/1000
+    console.log(now)
+
+    // Retrieve device telemetries
+    test = Device.scan().where('dateTime').ge(now-3600)
+      .exec((err, device) => {
+        console.log(device)
+      });
+
+/*
+    track.addEvent([
+            new MidiWriter.NoteEvent({pitch: ['21','22'], duration: '4'}),
+            new MidiWriter.NoteEvent({pitch: ['23'], duration: '2'}),
+            new MidiWriter.NoteEvent({pitch: ['24','25'], duration: '4'}),
+            new MidiWriter.NoteEvent({pitch: ['26'], duration: '2'}),
+            new MidiWriter.NoteEvent({pitch: ['27', 'C7', 'C7', 'C7', 'D7', 'D7', 'D7', 'D7'], duration: '8'}),
+            new MidiWriter.NoteEvent({pitch: ['E7','D7'], duration: '4'}),
+            new MidiWriter.NoteEvent({pitch: ['C7'], duration: '2'})
+        ], function(event, index) {
+        return {sequential: true};
+      }
+    );
+*/
+    var track = new MidiWriter.Track();
+    track.addEvent(NOTES_SEQUENCE);
+    const writer = new MidiWriter.Writer(track);
+    writer.saveMIDI('./prova');
+
+   console.log(req.decoded._id + " DEBUG END: endVisit");
+  } catch (e) {
+    console.error(req.decoded._id + " CATCH: endVisit : user error > " + e);
     return res.send({ success: false, status: 500, message: e.message });
   }
 };
